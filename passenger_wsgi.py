769617bc77e3320568a3136e2f99238c758bb333
Bibek Shrestha Map Server <bibekshrestha@gmail.com>
2013-08-18 22:43:19 +0200

    Working

---
+++ passenger_wsgi.py
@@ -0,0 +1,17 @@
+import sys, os
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/openeventmap')  #You must add your project here or 500
+
+#Switch to new python
+#You may try to replace $HOME with your actual path
+if sys.version < "2.7.3": os.execl("/home/bibek/app/virtualenv/openeventmapdjango/bin/python",
+    "python2.7.3", *sys.argv)
+
+sys.path.insert(0,'/home/bibek/app/virtualenv/openeventmapdjango/bin')
+sys.path.insert(0,'/home/bibek/app/virtualenv/openeventmapdjango/lib/python2.7/site-packages/django')
+sys.path.insert(0,'/home/bibek/app/virtualenv/openeventmapdjango/lib/python2.7/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "eventsvisualizer.settings"
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
